c3f89cc911ed106eaa6bc5419f22b99b5ee9b083
Bart <ioovmian@naver.com>
2021-11-21 14:56:58 +0900

    핸드러 정보 얻기 변경

--- myapp/app_test.go
+++ myapp/app_test.go
@@ -16,9 +16,40 @@ func TestIndexPathHandler(t *testing.T) {
 	res := httptest.NewRecorder()
 	req := httptest.NewRequest("Get", "/", nil)
 
-	indexHandler(res, req)
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
 
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("Hello World", string(data))
+}
+
+func TestBarPathHandler_WithoutName(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("Get", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("hello foo!! World", string(data))
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("Get", "/bar?name=bart", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("hello foo!! bart", string(data))
 }
