db7d1e1580a8c54e7097edc8ae03bc271a67cff4
krkwon <ioovmian@lsinvest.co.kr>
2021-11-22 20:48:28 +0900

    REST API 연동 테스트

--- myapp/app.go
+++ myapp/app.go
@@ -3,11 +3,44 @@ package myapp
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
+// NewHttpHandler make Handler
+func NewHttpHandler() http.Handler {
+	lastID = 0
+	userMap = make(map[int]*User)
+
+	mux := mux.NewRouter()
+	//mux := http.NewServeMux()
+
+	mux.HandleFunc("/index", indexHandler)
+
+	mux.HandleFunc("/index01", indexHandler01)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUsersHandler).Methods("POST")
+	mux.HandleFunc("/users/{id:[0-9]+}", getUsersInfoHandler).Methods("GET")
+	mux.HandleFunc("/users/{id:[0-9]+}", deleteUsersInfoHandler).Methods("DELETE")
+
+	mux.HandleFunc("/bar", barHandler)
+
+	mux.Handle("/foo", &fooHandler{})
+
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+
+	mux.HandleFunc("/uploads", uploadsHandler)
+	return mux
+}
+
+// User structure
 type User struct {
+	ID        int       `json:id`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
@@ -16,6 +49,85 @@ type User struct {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
+}
+
+func indexHandler01(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(userMap)
+	fmt.Fprint(w, string(data))
+}
+
+func deleteUsersInfoHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	_, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User Id : ", id)
+		return
+	}
+
+	delete(userMap, id)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Deleted User Id : ", id)
+}
+
+func getUsersInfoHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	user, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User Id : ", id)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
+var lastID int
+var userMap map[int]*User
+
+func createUsersHandler(w http.ResponseWriter, r *http.Request) {
+
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	lastID++
+	user.ID = lastID
+	user.CreatedAt = time.Now()
+	userMap[user.ID] = user
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
 type fooHandler struct{}
@@ -35,7 +147,7 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	data, _ := json.Marshal(user)
 	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, string(data))
 
 }
@@ -49,14 +161,27 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello foo!! %s", name)
 }
 
-func NewHttpHandler() http.Handler {
-	mux := http.NewServeMux()
+func uploadsHandler(w http.ResponseWriter, r *http.Request) {
+	uploadFile, header, err := r.FormFile("upload_file")
 
-	mux.HandleFunc("/", indexHandler)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	defer uploadFile.Close()
 
-	mux.HandleFunc("/bar", barHandler)
-
-	mux.Handle("/foo", &fooHandler{})
-
-	return mux
+	dirname := "./uploads"
+	os.MkdirAll(dirname, 0777)
+	filepath := fmt.Sprintf("%s/%s", dirname, header.Filename)
+	file, err := os.Create(filepath)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+	defer file.Close()
+	io.Copy(file, uploadFile)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, filepath)
 }
--- myapp/app_test.go
+++ myapp/app_test.go
@@ -1,9 +1,18 @@
 package myapp
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"io/ioutil"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,7 +23,7 @@ func TestIndexPathHandler(t *testing.T) {
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
-	req := httptest.NewRequest("Get", "/", nil)
+	req := httptest.NewRequest("Get", "/index", nil)
 
 	mux := NewHttpHandler()
 	mux.ServeHTTP(res, req)
@@ -40,7 +49,6 @@ func TestBarPathHandler_WithoutName(t *testing.T) {
 }
 
 func TestBarPathHandler_WithName(t *testing.T) {
-
 	assert := assert.New(t)
 
 	res := httptest.NewRecorder()
@@ -52,4 +60,153 @@ func TestBarPathHandler_WithName(t *testing.T) {
 	assert.Equal(http.StatusOK, res.Code)
 	data, _ := ioutil.ReadAll(res.Body)
 	assert.Equal("hello foo!! bart", string(data))
+}
+
+func TestFooHandler_WithoutJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("Get", "/foo", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusBadRequest, res.Code)
+}
+
+func TestFooHandler_WithJson(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("Get", "/foo",
+		strings.NewReader(`{"first_name" : "Bart", "last_name" : "kwon", "email" : "abcd@gmail.com" }`))
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	user := new(User)
+	err := json.NewDecoder(res.Body).Decode(user)
+	assert.Nil(err)
+	assert.Equal("Bart", user.FirstName)
+	assert.Equal("kwon", user.LastName)
+
+}
+
+func TestUpload(t *testing.T) {
+	assert := assert.New(t)
+	path := `C:\Users\Administrator\Pictures\LGTwins05.png`
+	file, _ := os.Open(path)
+	defer file.Close()
+
+	os.RemoveAll("./uploads")
+
+	buf := &bytes.Buffer{}
+	writer := multipart.NewWriter(buf)
+	multi, err := writer.CreateFormFile("upload_file", filepath.Base(path)) // filepath.Base(path) 파일명만 추출
+	assert.NoError(err)
+	io.Copy(multi, file)
+	writer.Close()
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/uploads", buf)
+	req.Header.Set("Content-type", writer.FormDataContentType())
+
+	uploadsHandler(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+
+	uploadFilePath := "./uploads/" + filepath.Base(path)
+	_, err01 := os.Stat(uploadFilePath)
+	assert.NoError(err01)
+
+	uploadFile, _ := os.Open(uploadFilePath)
+	originFile, _ := os.Open(path)
+	defer uploadFile.Close()
+	defer originFile.Close()
+
+	uploadData := []byte{}
+	originData := []byte{}
+
+	uploadFile.Read(uploadData)
+	uploadFile.Read(originData)
+
+	assert.Equal(originData, uploadData)
+}
+
+func TestIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/index01")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal("Hello World", string(data))
+}
+
+func TestGetUserInfo(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL + "/users/21")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "User Id")
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name" : "Bart", "last_name" : "kwon", "email" : "abcd@gmail.com" }`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/users/1", nil)
+	resp, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "Deleted User Id : 1")
+}
+
+func TestDeleteUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHttpHandler())
+	defer ts.Close()
+
+	req, _ := http.NewRequest("DELETE", ts.URL+"/users/1", nil)
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "Deleted User Id : 1")
+	log.Print(string(data))
 }
